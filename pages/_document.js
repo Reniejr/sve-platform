5b979b87c03e6278bdca25d462026b7c52b453a8
user.name: Personal <user.email: reniejr@gmail.com>
2023-09-28 17:11:32 +0200

    fix: font

--- pages/_document.js
+++ pages/_document.js
@@ -36,6 +36,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" 
             rel="stylesheet"
             />
+          <link href="https://fonts.googleapis.com/css2?family=Stardos+Stencil:wght@400;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
